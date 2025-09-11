4cb797b3e84ed749745d695e1210bc139cb2c786
Genki Miyauchi <g.miyauchi@sheffield.ac.uk>
2025-09-11 17:21:31 +0100

    Don't log practice run

--- loop_functions/target_tracking_loop_functions/target_tracking_loop_functions.cpp
+++ loop_functions/target_tracking_loop_functions/target_tracking_loop_functions.cpp
@@ -349,43 +349,49 @@ bool CTargetTrackingLoopFunctions::IsExperimentFinished() {
             LOG << "[LOG] Final Timestep: " << final_time << " steps = " << final_time_seconds << " seconds" << std::endl;
 
             /* Log experiment */
-            LOG << "[LOG] Logging experiment results to " << m_strLogFilePath << std::endl;
-
-            // Check if the heading is already present
-            std::ifstream infile(m_strLogFilePath);
-            bool has_heading = false;
-            std::string first_line;
-            if (std::getline(infile, first_line)) {
-                if (first_line.find("ID,TIME,SPEED,SEPARATION,BROADCAST,TARGET_X,TARGET_Y") != std::string::npos) {
-                    has_heading = true;
-                }
-            }
-            infile.close();
             
-            m_cOutput.open(m_strLogFilePath, std::ios_base::app);
-            if (!has_heading) {
-                m_cOutput << "ID,TIME,SPEED,SEPARATION,BROADCAST,TARGET_X,TARGET_Y";
-                for (size_t i = 0; i < m_vecRobotArrivalTime.size(); ++i) {
-                    m_cOutput << ",ARRIVAL_" << (i + 1);
+            UInt32 seed = CSimulator::GetInstance().GetRandomSeed();
+            UInt32 seed_practice = 10000; // Seed used for practice runs
+
+            if(seed != seed_practice) {
+                LOG << "[LOG] Logging experiment results to " << m_strLogFilePath << std::endl;
+
+                // Check if the heading is already present
+                std::ifstream infile(m_strLogFilePath);
+                bool has_heading = false;
+                std::string first_line;
+                if (std::getline(infile, first_line)) {
+                    if (first_line.find("ID,TIME,SPEED,SEPARATION,BROADCAST,TARGET_X,TARGET_Y") != std::string::npos) {
+                        has_heading = true;
+                    }
+                }
+                infile.close();
+                
+                m_cOutput.open(m_strLogFilePath, std::ios_base::app);
+                if (!has_heading) {
+                    m_cOutput << "ID,TIME,SPEED,SEPARATION,BROADCAST,TARGET_X,TARGET_Y";
+                    for (size_t i = 0; i < m_vecRobotArrivalTime.size(); ++i) {
+                        m_cOutput << ",ARRIVAL_" << (i + 1);
+                    }
+                    m_cOutput << "\n";
+                }
+
+                m_cOutput << CSimulator::GetInstance().GetRandomSeed() << ","
+                        << final_time_seconds << ","
+                        << m_fSpeed << ","
+                        << m_fSeparation << ","
+                        << m_fBroadcastDuration << ","
+                        << m_vecTargetsTemp[0].first.GetX() << ","
+                        << m_vecTargetsTemp[0].first.GetY();
+                for(const auto& arrival_time : m_vecRobotArrivalTime) {
+                    m_cOutput << "," << arrival_time;
                 }
                 m_cOutput << "\n";
-            }
+                
+                m_cOutput.close();
 
-            m_cOutput << CSimulator::GetInstance().GetRandomSeed() << ","
-                    << final_time_seconds << ","
-                    << m_fSpeed << ","
-                    << m_fSeparation << ","
-                    << m_fBroadcastDuration << ","
-                    << m_vecTargetsTemp[0].first.GetX() << ","
-                    << m_vecTargetsTemp[0].first.GetY();
-            for(const auto& arrival_time : m_vecRobotArrivalTime) {
-                m_cOutput << "," << arrival_time;
+                LOG << "[LOG] Experiment results logged successfully." << std::endl;
             }
-            m_cOutput << "\n";
-            
-            m_cOutput.close();
-
-            LOG << "[LOG] Experiment results logged successfully." << std::endl;
 
             return true;
         }
