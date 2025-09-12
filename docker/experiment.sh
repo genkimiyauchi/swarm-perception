e4d79bda7c676d2da3fb6cf8c38cce5939d5345d
Genki Miyauchi <g.miyauchi@sheffield.ac.uk>
2025-09-12 18:35:54 +0100

    Save participant ID

--- docker/experiment.sh
+++ docker/experiment.sh
@@ -2,13 +2,14 @@
 
 # Script to run commands during the webviz user study.
 #
-# Usage: ./experiment.sh <comamnd> <mode>
+# Usage: ./experiment.sh <command> <group> <ID>
 #
 #       where <command> is {run, save}
-#       where <mode> is {1, 2} (only when command is "run")
+#       where <group> is {1, 2} (only for "run")
+#       where <ID> is the participant ID (only for "save")
 
 if [ $# -eq 0 ]; then
-    echo "No command provided. Usage: ./experiment.sh <command={run,save}> <group={1,2}(only for run)>"
+    echo "No command provided. Usage: ./experiment.sh <command={run,save}> <group={1,2}(only for run)> <ID=participant ID(only for save)>"
     exit 1
 fi
 
@@ -26,14 +27,13 @@ if [ "$command" = "run" ]; then
     docker run --name argos -it -p 3000:3000 -p 8000:8000 -p 5000:5000 -v $(pwd)/swarm-perception-results:/home/docker/swarm-perception/results -w /home/docker/swarm-perception genki15/swarm-perception python3 web_app/app.py -m $mode
 
 elif [ "$command" = "save" ]; then
+    echo "Participant ID: $mode"
 
     RESULTS_DIR=~/Documents/swarm-perception-results
 
-    FILE_NAME=$(ls $RESULTS_DIR | grep .txt | head -n 1 | sed 's/\.txt//')
-
     # Check if the folder already exists
-    if [ -d "$RESULTS_DIR/$FILE_NAME" ]; then
-        echo "Directory $RESULTS_DIR/$FILE_NAME already exists. Do you want to overwrite its contents? (y/n)"
+    if [ -d "$RESULTS_DIR/$mode" ]; then
+        echo "Directory $RESULTS_DIR/$mode already exists. Do you want to overwrite its contents? (y/n)"
         read -r response
         if [[ "$response" != "y" ]]; then
             echo "Exiting without saving."
@@ -41,10 +41,10 @@ elif [ "$command" = "save" ]; then
         fi
     fi  
 
-    mkdir -p ~/Documents/swarm-perception-results/$FILE_NAME
+    mkdir -p ~/Documents/swarm-perception-results/$mode
 
-    echo "Saving results to $RESULTS_DIR/$FILE_NAME"
+    echo "Saving results to $RESULTS_DIR/$mode"
 
-    mv $RESULTS_DIR/*.* $RESULTS_DIR/$FILE_NAME/
+    mv $RESULTS_DIR/*.* $RESULTS_DIR/$mode/
 
 fi
